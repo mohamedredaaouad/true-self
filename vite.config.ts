57c7284e8ea4abf4d2768df1f1f096fc30cc8c08
mohamedredaaouad <mohamedredaaouad2001@gmail.com>
2026-06-06 14:12:41 +0100

    supriimer quelque choe dans vite config

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,6 @@ export default defineConfig({
     // Redirect TanStack Start's bundled server entry to src/server.ts (our SSR error wrapper).
     // nitro/vite builds from this
     server: { entry: "server" },
-    base: process.env.VITE_BASE_PATH || "/pure-calm-studio" ,
   
   },
 });
